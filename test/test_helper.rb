1cda800dfa48502054dbfe3c946650bfd739cb22
Christos Trochalakis <yatiohi@ideopolis.gr>
2015-10-25 15:27:57 +0200

    test: Add pattern to hook custom test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+Dir[Rails.root.join("test/support/*.rb")].each {|f| require f }
+
 class ActiveSupport::TestCase
   include FactoryGirl::Syntax::Methods
 end
