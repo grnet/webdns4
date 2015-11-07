313d7a3fb9d9e13e4b9f9a7842d3a13a316e9da7
Christos Trochalakis <yatiohi@ideopolis.gr>
2015-11-07 14:10:49 +0200

    Implement a simple drop privileges validator
    This is a trick to delegate a privilege validations from the controller
    to the model so we can handle them uniformly through the rails model
    validation flow.
    
    This is achieved by setting the record in `dropped_privileges` mode,
    just set `dropped_privilages = true`.

---
+++ lib/drop_privileges_validator.rb
@@ -0,0 +1,27 @@
+module ActiveModel
+  module Validations
+    class DropPrivilegesValidator < EachValidator
+      def initialize(options)
+        super
+        setup!(options[:class])
+      end
+
+      def validate_each(record, attribute, _value)
+        record.errors.add(attribute, options[:message]) if record.drop_privileges
+      end
+
+      private
+
+      def setup!(klass)
+        klass.send(:attr_reader, :drop_privileges) unless klass.method_defined?(:drop_privileges)
+        klass.send(:attr_writer, :drop_privileges) unless klass.method_defined?(:drop_privileges=)
+      end
+    end
+
+    module HelperMethods
+      def validates_drop_privileges(*attr_names)
+        validates_with DropPrivilegesValidator, _merge_attributes(attr_names)
+      end
+    end
+  end
+end
