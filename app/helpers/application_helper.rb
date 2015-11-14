8525d0ada430138814a023f6d0cb25c0d721e424
Christos Trochalakis <yatiohi@ideopolis.gr>
2015-11-14 13:03:04 +0200

    Use glyphicons instead of text for edit/enable/disable actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,31 @@ module ApplicationHelper
 
     object.editable?
   end
+
+  def link_to_edit(*args, &block)
+    link_to(abbr_glyph(:pencil, 'Edit'), *args, &block)
+  end
+
+  def link_to_destroy(*args, &block)
+    link_to(abbr_glyph(:remove, 'Remove'), *args, &block)
+  end
+
+  def link_to_enable(*args, &block)
+    link_to(abbr_glyph(:'eye-close', 'Enable'), *args, &block)
+  end
+
+  def link_to_disable(*args, &block)
+    link_to(abbr_glyph(:'eye-open', 'Disable'), *args, &block)
+  end
+
+  def glyph(icon)
+    content_tag(:span, '', class: "glyphicon glyphicon-#{icon}")
+  end
+
+  private
+
+  def abbr_glyph(icon, title)
+    content_tag(:abbr, glyph(icon), title: title)
+  end
+
 end
