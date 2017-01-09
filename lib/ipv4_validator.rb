3407f0fdcb92dfdd36bda5722f967ec9f1e8b4f0
Christos Trochalakis <yatiohi@ideopolis.gr>
2017-01-09 09:13:21 +0200

    Simplify IP validators
    Make the 2 validators reusable and use a common API.

--- lib/ipv4_validator.rb
+++ lib/ipv4_validator.rb
@@ -3,7 +3,7 @@ require 'socket'
 
 class Ipv4Validator < ActiveModel::EachValidator
   # Returns true if addr is a valid IPv4 address.
-  def valid_v4?(addr)
+  def self.valid?(addr)
     return false if addr['/'] # IPAddr accepts addr/mask format
     IPAddr.new(addr, Socket::AF_INET)
     true
@@ -13,7 +13,8 @@ class Ipv4Validator < ActiveModel::EachValidator
 
   # Add an attribute error if this is not a valid IPv4 address.
   def validate_each(record, attribute, value)
-    return if valid_v4?(value)
+    return if Ipv4Validator.valid?(value)
+
     record.errors[attribute] << 'is not a valid IPv4 address'
   end
 end
--- lib/ipv6_validator.rb
+++ lib/ipv6_validator.rb
@@ -3,7 +3,7 @@ require 'socket'
 
 class Ipv6Validator < ActiveModel::EachValidator
   # Returns true if addr is valid IPv6 address.
-  def valid_v6?(addr)
+  def self.valid?(addr)
     return false if addr['/'] # IPAddr accepts addr/mask format
     return false if addr['[']
     return false if addr[']']
@@ -15,7 +15,7 @@ class Ipv6Validator < ActiveModel::EachValidator
 
   # Add an attribute error if this is not a valid IPv4 address.
   def validate_each(record, attribute, value)
-    return if valid_v6?(value)
+    return if Ipv6Validator.valid?(value)
 
     record.errors[attribute] << 'is not a valid IPv6 address'
   end
