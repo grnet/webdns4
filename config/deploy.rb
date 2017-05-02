69b46f2909121d562a3b5bc82ad8562ad8f17605
Alexandros Afentoulis <alexaf@noc.grnet.gr>
2017-05-02 18:31:43 +0300

    Changes deploy git url to repo.grnet
    Summary: Changes deploy git url to repo.grnet
    
    Test Plan: not
    
    Reviewers: alexk
    
    Reviewed By: alexk
    
    Subscribers: #serversnoc
    
    Differential Revision: https://repo.grnet.gr/D9

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'webdns'
-set :repo_url, 'git@bitbucket.org:xlembouras/webns.git'
+set :repo_url, 'https://repo.grnet.gr/diffusion/WEBDNS/webdns.git'
 
 set :deploy_to, '/srv/webdns'
 
