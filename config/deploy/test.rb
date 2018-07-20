c7665c5fe8853e2302aec1a3bc58f2dbf2aabd34
Christos Karagiannis <ckaragiannis@noc.grnet.gr>
2018-07-20 13:14:21 +0300

    Add custom ssh port for deploy through ci

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -2,4 +2,4 @@ set :rails_env, :production
 
 server 'webdns4.test.grnet.gr', user: 'webdns', roles: %w(web app db)
 
-set :ssh_options, forward_agent: false
+set :ssh_options, forward_agent: false, port: 29
