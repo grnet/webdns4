57b5e702724d574a7e80f0448810edeb167d4ff9
Christos Karagiannis <ckaragiannis@noc.grnet.gr>
2018-07-20 13:17:47 +0300

    Update ci deployer user

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -1,5 +1,5 @@
 set :rails_env, :production
 
-server 'webdns4.test.grnet.gr', user: 'webdns', roles: %w(web app db)
+server 'webdns4.test.grnet.gr', user: 'deployer', roles: %w(web app db)
 
 set :ssh_options, forward_agent: false, port: 29
