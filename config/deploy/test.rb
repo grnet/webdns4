f7995887a2eeed84735130e8ad2ae59db173b7b0
Christos Karagiannis <ckaragiannis@noc.grnet.gr>
2018-07-20 17:54:53 +0300

    Add overrides for rm and ln commands to include sudo, for ci deployment user

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -3,3 +3,8 @@ set :rails_env, :production
 server 'webdns4.test.grnet.gr', user: 'deployer', roles: %w(web app db)
 
 set :ssh_options, forward_agent: false, port: 29
+
+#Override rm and ln commands with sudo, for use with jenkins deployer
+SSHKit.config.command_map[:rm] = 'sudo rm'
+SSHKit.config.command_map[:ln] = 'sudo ln'
+
