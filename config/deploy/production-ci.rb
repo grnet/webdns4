01785223a461d898d5c64ff12ba7a7af6d41b2f9
Christos Karagiannis <ckaragiannis@noc.grnet.gr>
2018-09-13 15:15:57 +0300

    Disable ssh override on ci for webdns4 production

--- config/deploy/production-ci.rb
+++ config/deploy/production-ci.rb
@@ -5,7 +5,7 @@ server 'webdns4.grnet.gr', user: 'deployer', roles: %w(web app db)
 set :ssh_options, forward_agent: false, port: 29
 
 #Override commands with sudo, for use with jenkins deployer
-SSHKit.config.command_map = Hash.new do |hash, command|
-  hash[command] = "sudo -u webdns #{command}"
-end
+#SSHKit.config.command_map = Hash.new do |hash, command|
+#  hash[command] = "sudo -u webdns #{command}"
+#end
 
