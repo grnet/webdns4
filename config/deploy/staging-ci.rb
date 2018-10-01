ab72bf12c7524bd511ad68b73c7ae7631d088340
Vitalis Salis <vitsalis@gmail.com>
2018-10-01 18:08:29 +0300

    Add branch environment variable

--- config/deploy/staging-ci.rb
+++ config/deploy/staging-ci.rb
@@ -2,4 +2,6 @@ set :rails_env, :production
 
 server 'webdns4.staging.grnet.gr', user: 'deployer', roles: %w(web app db)
 
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
+
 set :ssh_options, forward_agent: false, port: 29
